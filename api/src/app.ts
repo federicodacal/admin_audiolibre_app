9da797c902370639be261c42f941faa3bac38769
Federico Dacal <federicodacal@gmail.com>
2024-10-09 00:17:59 -0300

    prueba deploy

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,9 +3,6 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 
-import * as middlewares from './middlewares';
-import MessageResponse from './interfaces/MessageResponse';
-
 require('dotenv').config();
 
 const app = express();
@@ -17,15 +14,6 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
-});
-
 app.use('/api/', suscripcionRoutes);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
 
 export default app;
