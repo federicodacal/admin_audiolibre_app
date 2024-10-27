1f2c97a06e7218ddacfa90f7d13a5f5a91bf81c2
Federico Dacal <federicodacal@gmail.com>
2024-10-27 01:38:49 -0300

    middleware jwt routes

--- api/src/routes/suscripcion.routes.js
+++ api/src/routes/suscripcion.routes.js
@@ -4,9 +4,9 @@ const { cookieJwtAuth } = require("../middlewares/jwt");
 
 const suscripcionesRoutes = Router();
 
-suscripcionesRoutes.get('/suscripciones', suscripcionController.obtener_suscripciones);
+suscripcionesRoutes.get('/suscripciones', cookieJwtAuth, suscripcionController.obtener_suscripciones);
 suscripcionesRoutes.get('/suscripciones/:id', cookieJwtAuth, suscripcionController.obtener_suscripcion_por_id);
-suscripcionesRoutes.get('/nuevaSuscripcion', suscripcionController.mostrar_view_crear);
+suscripcionesRoutes.get('/nuevaSuscripcion', cookieJwtAuth, suscripcionController.mostrar_view_crear);
 suscripcionesRoutes.post('/nuevaSuscripcion', cookieJwtAuth, suscripcionController.crear_suscripcion);
 suscripcionesRoutes.put('/suscripciones/modificar/:id', cookieJwtAuth, suscripcionController.actualizar_suscripcion);
 suscripcionesRoutes.delete('/suscripciones/eliminar/:id', cookieJwtAuth, suscripcionController.eliminar_suscripcion);
